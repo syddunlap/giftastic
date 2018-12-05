cfb716fe31acd023cae6a7502fc9e57aac30f05b
Sydney Dunlap <sydneydunlap@Sydneys-MacBook-Pro.local>
2018-12-05 18:10:32 -0500

    changed a few things

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,66 @@
-// Initial array of moview
-var movies = ["Aristocats", "The Little Mermaid", "Snow White", "Cars"];
+$(document).ready(function () {
 
-// A function to render the HTML to display the appropriate gif info
-function displayMovieGif () {
-    var movie = $(this).attr("data-name");
-    var queryURL = "";
+    // Initial array of moview
+    var movies = ["Aristocats", "The Little Mermaid", "Snow White", "Cars"];
 
-    // An AJAX call for the specific movie button being clicked
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        
-        // Lots more to add hereeeeeee
-        // ----------
-        // ----------
-        // ----------
-    })
-}
+    // A function to render the HTML to display the appropriate gif info
+    function displayMovieGif() {
+        var movie = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=YOURAPIKEY&limit=10";
 
-// Function for printing the buttons to the page
-function renderButtons() {
+        // An AJAX call for the specific movie button being clicked
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
 
-    // Deleting the movies prior to adding new movies is necessary otherwise we will have repeat buttons
-    $("#buttons-view").empty();
-
-    // Loop through the array of movies
-    for (var i = 0; i < movies.length; i++) {
-        
-        // Dynamically generate buttons for each movie in the array
-        var a = $("<button>");
-        
-        // Add a class of movie to our button
-        a.addClass("movie");
-        
-        // Add a data-attribute
-        a.attr("data-name", movies[i]);
-        
-        // Providing the initial button text
-        a.text(movies[i]);
-        
-        // Printint buttons to the HTML
-        $("#buttons-view").append(a);
+            // Lots more to add hereeeeeee
+            // ----------
+            // ----------
+            // ----------
+        })
     }
 
-    // A click event function for the Submit button
-    $("#add-movie").on("click", function(event) {
+    // Function for printing the buttons to the page
+    function renderButtons() {
 
-        // Prevent the button from submitting a form
-        event.preventDefault();
+        // Deleting the movies prior to adding new movies is necessary otherwise we will have repeat buttons
+        $("#buttons-view").empty();
 
-        // Grab the input from the textbox
-        var movie = $("#userInput").val().trim();
+        // Loop through the array of movies
+        for (var i = 0; i < movies.length; i++) {
 
-        // Add the movie from the textbox to our array
-        movies.push(movie);
+            // Dynamically generate buttons for each movie in the array
+            var a = $("<button>");
 
-        // Calling our renderButtons function to print the new movie array
-        renderButtons();
-    })
+            // Add a class of movie to our button
+            a.addClass("movie");
 
-}
+            // Add a data-attribute
+            a.attr("data-name", movies[i]);
+
+            // Providing the initial button text
+            a.text(movies[i]);
+
+            // Printint buttons to the HTML
+            $("#buttons-view").append(a);
+        }
+
+        // A click event function for the Submit button
+        $("#add-movie").on("click", function (event) {
+
+            // Prevent the button from submitting a form
+            event.preventDefault();
+
+            // Grab the input from the textbox
+            var movie = $("#userInput").val().trim();
+
+            // Add the movie from the textbox to our array
+            movies.push(movie);
+
+            // Calling our renderButtons function to print the new movie array
+            renderButtons();
+        })
+
+    }
+})
