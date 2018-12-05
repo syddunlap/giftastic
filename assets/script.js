506cf6cfa97e276f3182539d6e95f3fc24d58050
Sydney Dunlap <sydneydunlap@Sydneys-MacBook-Pro.local>
2018-12-05 17:14:53 -0500

    Some HTML Bootstrap work and the beginnings of JavaScript file

---
+++ assets/script.js
@@ -0,0 +1,51 @@
+// Initial array of moview
+var movies = ["Aristocats", "The Little Mermaid", "Snow White", "Cars"];
+
+// Generic function for capturing the movie name from the data-attribute
+function alertMovieName() {
+    var movieName = $(this).attr("data-name");
+    alert(movieName);
+}
+
+// Function for printing the buttons to the page
+function renderButtons() {
+
+    // Deleting the movies prior to adding new movies is necessary otherwise we will have repeat buttons
+    $("#buttons-view").empty();
+
+    // Loop through the array of movies
+    for (var i = 0; i < movies.length; i++) {
+        
+        // Dynamically generate buttons for each movie in the array
+        var a = $("<button>");
+        
+        // Add a class of movie to our button
+        a.addClass("movie");
+        
+        // Add a data-attribute
+        a.attr("data-name", movies[i]);
+        
+        // Providing the initial button text
+        a.text(movies[i]);
+        
+        // Printint buttons to the HTML
+        $("#buttons-view").append(a);
+    }
+
+    // A click event function for the Submit button
+    $("#add-movie").on("click", function(event) {
+
+        // Prevent the button from submitting a form
+        event.preventDefault();
+
+        // Grab the input from the textbox
+        var movie = $("#userInput").val().trim();
+
+        // Add the movie from the textbox to our array
+        movies.push(movie);
+
+        // Calling our renderButtons function to print the new movie array
+        renderButtons();
+    })
+
+}
